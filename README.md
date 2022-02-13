22bc8ddc2ee9969aa49d2a7bc3dffa72260899ae
Thomas Boyle <4200802+tom-xyz@users.noreply.github.com>
2022-02-13 15:35:28 +0000

    Initial docs and model file

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # 3DSimpleTurntable
 3D Model files and source code for rotating turntable. Raspberry Pi, DC servo and PWM modulator required.
+
+## Preview
+![](https://github.com/Tom-xyz/3DSimpleTurntable/blob/main/Preview/preview.gif)
+
+
+## Construction
+
+
+### Printing
+3D Model Files are available in the following directory: '3D Model'
+* Print the base ('v0_stepper_rotator_base.obj')
+* Print the motor fixture ('v0_stepper_rotator_lid.obj')
+* Print the top/turntable (TODO)
+
+![](https://github.com/Tom-xyz/3DSimpleTurntable/blob/main/Preview/3d_print_1.jpg)
+![](https://github.com/Tom-xyz/3DSimpleTurntable/blob/main/Preview/3d_print_2.jpg)
+
+
+### Assembly
+
+TODO: Wiring Diagrams
+
+TODO: Parts list
+
+
+![](https://github.com/Tom-xyz/3DSimpleTurntable/blob/main/Preview/internals.jpg)
+
+
+![](https://github.com/Tom-xyz/3DSimpleTurntable/blob/main/Preview/motor_lid.jpg)
+
+
+![](https://github.com/Tom-xyz/3DSimpleTurntable/blob/main/Preview/wiring.jpg)
+
+
+
+### Demo
+
+Source code for the stepper motor servos is provided in the 'src' directory.
+
+Upload the Python script to your Raspberry pi and invoke it:
+`python3 StepperMotorDemo.py`
---
+++ src/StepperMotorDemo.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+import RPi.GPIO as GPIO
+import time
+
+# GPIO PINs, map as needed
+in1 = 17
+in2 = 18
+in3 = 27
+in4 = 22
+
+# careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
+step_sleep = 0.002
+
+step_count = 4096  # 4096 steps is 360°
+
+clockwise = True
+
+# Stepper motor sequence
+step_sequence = [[1, 0, 0, 1],
+                 [1, 0, 0, 0],
+                 [1, 1, 0, 0],
+                 [0, 1, 0, 0],
+                 [0, 1, 1, 0],
+                 [0, 0, 1, 0],
+                 [0, 0, 1, 1],
+                 [0, 0, 0, 1]]
+
+# Setup GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(in3, GPIO.OUT)
+GPIO.setup(in4, GPIO.OUT)
+
+GPIO.output(in1, GPIO.LOW)
+GPIO.output(in2, GPIO.LOW)
+GPIO.output(in3, GPIO.LOW)
+GPIO.output(in4, GPIO.LOW)
+
+motor_pins = [in1, in2, in3, in4]
+motor_step_counter = 0
+
+
+def cleanup():
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.LOW)
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.LOW)
+    GPIO.cleanup()
+
+
+try:
+    i = 0
+    for i in range(step_count):
+        for pin in range(0, len(motor_pins)):
+            GPIO.output(motor_pins[pin], step_sequence[motor_step_counter][pin])
+        if clockwise:
+            motor_step_counter = (motor_step_counter - 1) % 8
+        elif not clockwise:
+            motor_step_counter = (motor_step_counter + 1) % 8
+        time.sleep(step_sleep)
+
+except KeyboardInterrupt:
+    cleanup()
+    exit(1)
+
+cleanup()
+exit(0)
